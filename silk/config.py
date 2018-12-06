fb1104e63df54996ec68b0725f21ae0022062d21
Watte <patrick.watzlawik@web.de>
2018-12-06 11:40:26 +0100

    default-database: Add SilkDBRouter and another silky option "SILKY_DATABASE_NAME"
    The SilkDBRouter will permit model operations of silk application only to the database defined in SILKY_DATABASE_NAME.
    The default is "default" database. Additionally, SilkyConfig will check if SilkDBRouter is set in django settings when SILKY_DATABASE_NAME
    differs from "default". This way can tell the user if his project is improperly configured.

--- silk/config.py
+++ silk/config.py
@@ -1,5 +1,6 @@
 from copy import copy
 
+from django.core.exceptions import ImproperlyConfigured
 from django.utils import six
 
 from silk.singleton import Singleton
@@ -21,14 +22,15 @@ class SilkyConfig(six.with_metaclass(Singleton, object)):
         'SILKY_AUTHENTICATION': False,
         'SILKY_AUTHORISATION': False,
         'SILKY_PERMISSIONS': default_permissions,
-        'SILKY_MAX_RECORDED_REQUESTS': 10**4,
+        'SILKY_MAX_RECORDED_REQUESTS': 10 ** 4,
         'SILKY_MAX_RECORDED_REQUESTS_CHECK_PERCENT': 10,
         'SILKY_MAX_REQUEST_BODY_SIZE': -1,
         'SILKY_MAX_RESPONSE_BODY_SIZE': -1,
         'SILKY_INTERCEPT_PERCENT': 100,
         'SILKY_INTERCEPT_FUNC': None,
         'SILKY_PYTHON_PROFILER': False,
-        'SILKY_STORAGE_CLASS': 'silk.storage.ProfilerResultStorage'
+        'SILKY_STORAGE_CLASS': 'silk.storage.ProfilerResultStorage',
+        'SILKY_DATABASE_NAME': 'default'
     }
 
     def _setup(self):
@@ -38,6 +40,19 @@ class SilkyConfig(six.with_metaclass(Singleton, object)):
         self.attrs = copy(self.defaults)
         self.attrs['SILKY_PYTHON_PROFILER_RESULT_PATH'] = settings.MEDIA_ROOT
         self.attrs.update(options)
+
+        if self.attrs["SILKY_DATABASE_NAME"] != "default":
+            self._check_database_routers(settings.DATABASE_ROUTERS)
+
+    def _check_database_routers(self, routers):
+        silk_router_present = False
+
+        for r in routers:
+            if r == "silk.routers.SilkDBRouter":
+                silk_router_present = True
+
+        if not silk_router_present:
+            raise ImproperlyConfigured("When using setting 'SILKY_DATABASE_NAME', also add 'silk.routers.SilkDBRouter' to your 'DATABASE_ROUTERS'.")
 
     def __init__(self):
         super(SilkyConfig, self).__init__()
