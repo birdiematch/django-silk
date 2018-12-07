058d0fcee5b92bedf98b6d50dd3172564c18143c
Watte <patrick.watzlawik@web.de>
2018-12-07 15:23:47 +0100

    setup.py: Increase version, update properties

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from setuptools import setup
 try:
     from pypandoc import convert
 
+
     def read_md(f):
         return convert(f, 'rst')
 except ImportError:
     print("warning: pypandoc module not found, could not convert Markdown to RST")
+
 
     def read_md(f):
         return open(f, 'r').read()
@@ -17,32 +19,23 @@ README = read_md('README.md')
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-
 setup(
     name='django-silk',
-    version='3.0.1',
+    version='3.1.0',
     packages=['silk'],
     include_package_data=True,
     license='MIT License',
     description='Silky smooth profiling for the Django Framework',
     long_description=README,
-    url='http://www.mtford.co.uk/projects/silk/',
-    author='Michael Ford',
-    author_email='mtford@gmail.com',
+    url='https://github.com/birdiematch/django-silk/',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
